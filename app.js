e7aaad093ea5f9afea2f53f14f7b3adbfbb878a2
FRANCEBEAM <santanderfrancisbeam@gmail.com>
2023-05-07 17:33:41 +0800

    form validation v2

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ function validateFirstName() {
          iconValid.classList.add('fa-solid', 'fa-circle-check');
          firstNameDialog.appendChild(iconValid)
 
-        return true;
-
+        return firstNameInput;
     }
+
 }
 
 // #-----------Last name input validation
@@ -106,7 +106,7 @@ function validateLastName() {
         validIcon.classList.add('fa-solid', 'fa-circle-check');
         lastNameDialog.appendChild(validIcon);
 
-        return true;
+        return lastNameInput;
 
     }
 }
@@ -116,7 +116,7 @@ function validateLastName() {
 function validateEmail() {
     let emailInput = document.querySelector('#email-field').value;
 
-    if(emailInput === '') {
+    if(emailInput.trim() === '') {
 
         // Show no message
         let emailDialog = document.querySelector('.email-dialog');
@@ -189,7 +189,7 @@ function validateEmail() {
          validIcon.classList.add('fa-solid', 'fa-circle-check');
          emailDialog.appendChild(validIcon);
 
-        return validateEmail;
+        return emailInput;
 
     }
 }
@@ -199,7 +199,7 @@ function validateEmail() {
 function validatePassword(){
     const passInputField = document.querySelector('#password-field').value;
 
-    if (passInputField === '') {
+    if (passInputField.trim() === '') {
 
         //Remove Error Message
         let passwordDialog = document.querySelector('.password-dialog');
@@ -256,7 +256,7 @@ function validatePassword(){
         passInputBoarder.classList.add('valid');
 
 
-        return true;
+        return passInputField;
 
     }
 }
@@ -269,7 +269,7 @@ function validateConPassword(){
     const conPassword = document.querySelector('.conpassword')
     const conPasswordDialog = document.querySelector('.conpassword-dialog');
 
-    if (conPasswordInput === '') {
+    if (conPasswordInput.trim() === '') {
 
         // Remove any message once empty
         conPasswordDialog.innerHTML = '';
@@ -317,7 +317,7 @@ function validateConPassword(){
         validIcon.classList.add('fa-solid', 'fa-circle-check');
         conPasswordDialog.append(validIcon);
 
-        return true;
+        return conPasswordInput;
 
     }
 }
@@ -330,7 +330,7 @@ function validateCountry(){
     const countryInput = document.querySelector('.country');
     const countryDialog = document.querySelector('.country-dialog');
 
-    if (countryInputField === '') {
+    if (countryInputField.trim() === '') {
 
         // Remove show error message
         countryDialog.innerHTML = '';
@@ -365,6 +365,8 @@ function validateCountry(){
         //Remove Error message
         countryDialog.innerHTML = '';
 
+        countryInput.classList.remove('error');
+
         // Show valid Boarder
         countryInput.classList.add('valid');
 
@@ -374,7 +376,7 @@ function validateCountry(){
          countryDialog.appendChild(validIcon);
  
 
-        return true;
+        return countryInputField;
 
     }
 }
@@ -386,7 +388,7 @@ function validatePhone(){
     const inputPhone = document.querySelector('.phonenumber');
     const phoneDialog = document.querySelector('.phonenum-dialog')
 
-    if(inputPhoneField === ''){
+    if(inputPhoneField.trim() === ''){
 
         // Remove Boarder Error
         inputPhone.classList.remove('error');
@@ -430,17 +432,17 @@ function validatePhone(){
         validIcon.classList.add('fa-solid', 'fa-circle-check');
         phoneDialog.appendChild(validIcon)
 
-        return true;
+        return inputPhoneField;
 
     }
 }
 
 
+// CHECK ALL FIELD SUBMIT FORM
+const form = document.querySelector('#form');
 
-// FORM EVENT
-let form = document.querySelector('.btn-signup');
-
-form.addEventListener('click', (e)=>{
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
 
     const isFirstNameValid = validateFirstName();
     const isLastNameValid = validateLastName();
@@ -451,14 +453,162 @@ form.addEventListener('click', (e)=>{
     const isPhoneNumberValid = validatePhone();
 
 
+    // CHECK ALL FIELD
+
+    if(isFirstNameValid == '') {
+        const firstNameBoarder = document.querySelector('.firstname');
+        const firstNameDialog = document.querySelector('.firstname-dialog');
+
+        firstNameBoarder.classList.add('error');
+        firstNameDialog.innerHTML = 'First name is required*';
+
+    } else {
+        console.log('first name is ok')
+    }
+
+    if(isLastNameValid == '') {
+        const lastNameBoarder = document.querySelector('.lastname');
+        const lastNameDialog = document.querySelector('.lastname-dialog');
+
+        lastNameBoarder.classList.add('error');
+        lastNameDialog.innerHTML = 'Last name is required*';
+
+    } else {
+        console.log('last name is ok')
+    }
+
+
+    if(isEmailValid == '') {
+        const emailBoarder = document.querySelector('.email');
+        const emailDialog = document.querySelector('.email-dialog');
+
+        emailBoarder.classList.add('error');
+        emailDialog.innerHTML = 'Email is required*';
+
+    } else {
+        console.log('email is ok');
+    }
+
+
+    if(isPasswordValid == '') {
+        const passBoarder = document.querySelector('.password');
+        const passDialog = document.querySelector('.password-dialog');
+
+        passBoarder.classList.add('error');
+        passDialog.innerHTML = 'Password is required*';
+        
+    } else {
+        console.log('password is ok');
+    }
+
+
+    if(isConPassValid == '') {
+        const conPassBoarder = document.querySelector('.conpassword');
+        const conPassDialog = document.querySelector('.conpassword-dialog');
+
+        conPassBoarder.classList.add('error');
+        conPassDialog.innerHTML = 'Confirm Password is required*';
+
+    } else {
+        console.log('confirm password is ok');
+    }
+
+    if(isCountryValid == '') {
+        const countryBoarder = document.querySelector('.country');
+        const countryDialog = document.querySelector('.country-dialog');
+
+        countryBoarder.classList.add('error');
+        countryDialog.innerHTML = 'Country is required*';
+
+    } else {
+        console.log('country is ok');
+    }
+
+    if(isPhoneNumberValid == '') {
+        const phoneNumberBoarder = document.querySelector('.phonenumber');
+        const phoneNumberDialog = document.querySelector('.phonenum-dialog');
+
+        phoneNumberBoarder.classList.add('error');
+        phoneNumberDialog.innerHTML = 'Phone number is required*';
+
+    } else {
+        console.log('phone number is ok');
+    }
+
+
+    // VALIDATE FORM
+
     if ( isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid && isConPassValid && isCountryValid && isPhoneNumberValid) {
 
-        alert("Welcome new User!")
+        alert("Welcome new User!");
+
+        // FIRST NAME FIELD
+        let firstNameField = document.querySelector('.firstname');
+        firstNameField.value = ""
+        firstNameField.classList.remove('valid')
+        firstNameField.classList.remove('error')
+
+        let firstNameDialog = document.querySelector('.firstname-dialog')
+        firstNameDialog.innerHTML = '';
+
+        // LAST NAME FIELD
+        let lastNameField = document.querySelector('.lastname');
+        lastNameField.value = ""
+        lastNameField.classList.remove('valid')
+        lastNameField.classList.remove('error')
+
+        let lastNameDialog = document.querySelector('.lastname-dialog')
+        lastNameDialog.innerHTML = '';
+
+        //EMAIL FIELD
+        let emailField = document.querySelector('.email');
+        emailField.value = ""
+        emailField.classList.remove('valid')
+        emailField.classList.remove('error')
+
+        let emailDialog = document.querySelector('.email-dialog')
+        emailDialog.innerHTML = '';
+
+        // PASSWORD FIELD
+        let passwordField = document.querySelector('.password');
+        passwordField.value = ""
+        passwordField.classList.remove('valid')
+        passwordField.classList.remove('error')
+
+        let passwordDialog = document.querySelector('.password-dialog')
+        passwordDialog.innerHTML = '';
+
+        // CONFIRM PASSWORD FIELD
+        let conpasswordField = document.querySelector('.conpassword');
+        conpasswordField.value = ""
+        conpasswordField.classList.remove('valid')
+        conpasswordField.classList.remove('error')
+
+        let conpasswordDialog = document.querySelector('.conpassword-dialog')
+        conpasswordDialog.innerHTML = '';
+
+        //COUNTRY FIELD
+        let countryField = document.querySelector('.country');
+        countryField.value = ""
+        countryField.classList.remove('valid')
+        countryField.classList.remove('error')
+
+        let countryDialog = document.querySelector('.country-dialog')
+        countryDialog.innerHTML = '';
+
+        //PHONE NUMBER FIELD
+        let phoneNumberField = document.querySelector('.phonenumber');
+        phoneNumberField.value = ""
+        phoneNumberField.classList.remove('valid')
+        phoneNumberField.classList.remove('error')
+
+        let phoneNumberDialog = document.querySelector('.phonenum-dialog')
+        phoneNumberDialog.innerHTML = '';
 
     } else {
 
         alert("Fill out all required field");
 
     }
-})
 
+})
